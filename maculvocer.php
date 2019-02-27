11414ec3a8172f54716d2a7d8472e2cb5ebc38a2
godeyes333 <44834023+godeyes333@users.noreply.github.com>
2019-02-27 15:37:18 +0700

    Create maculvocer.php

---
+++ maculvocer.php
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+figlet MACULVOCER.PHP |lolcat
+
+echo "{×}==================================={×}"|lolcat
+echo "Creator: godeyescyber.com"|lolcat
+echo "-----------------------------------------"|lolcat
+echo "Team   : 1.godeyes cyber team"|lolcat
+echo "         2.succesfull ndeso families"|lolcat
+echo "-----------------------------------------"|lolcat
+echo "Thanks : > KEMINFO|lolcat
+echo "         > Police of Termux |lolcat
+echo "         > KOMISIONER TERMUX SEJAWA"|lolcat
+echo "{×}==================================={×}"|lolcat
+echo ""   
+echo "Oke langsung mawon nggih sederek "|lolcat
+echo "___________________________________________________"|lolcat
+echo ""
+echo "1.> MACUL PULSA KULO   "$red
+echo ""
+echo "2.> MACUL PULSA RENCANG     "$green
+echo "___________________________________________________"|lolcat
+
+echo ""
+echo "\033[34;1m  
+99\033[37;1m.\033[32;1mKeluar\033[37;1m"
+echo ""
+echo "\033[32;1m ──> \033[32;1m Pilih langsung wae rasah cerewet!\033[32;1m"
+read -p " PILIHAN=> " pilih
+if [ $pilih = 1 ]
+then
+clear
+figlet MACUL PULSA | lolcat
+echo "\033[34;1msabar ojo kesusu"
+echo ""
+sleep 1
+
+echo "arep macul pulsa piro.?"
+read -p "(ex:5000, 10000 dan seterusnya)=> " pulsa
+echo ""
+echo ""
+echo "nomor lu"
+read -p "nomor pake +62 eaa =>" nomor
+echo "Sabar nggih lagi ngirim kii"  
+echo ""
+echo ""
+echo "jumlah pulsa :" $pulsa|lolcat
+echo "ke nomer :" $nomor |lolcat
+echo ""
+echo ""
+sleep 2
+clear
+figlet LOADING|lolcat
+sleep 15
+echo ""
+echo "Berhasil macul pulsa anda 100%"|lolcat
+echo ""
+sleep 2
+echo "selamat pengisian pulsa anda sukses"|lolcat
+echo "matikan segera android anda tunggu 5 menit"|lolcat
+echo ""
+sleep 2
+echo "ya uda makasih eaa besuk jangan ngisi lagi"|lolcat
+echo "pesan gw"|lolcat
+sleep 2
+echo "kalean semua BURIQQ, gw doang yg GANNSS"|lolcat
+echo "created by godeyes cyber"|lolcat
+sleep 2
+figlet B . Y . E|lolcat
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+figlet SEND PULSA | lolcat
+echo "\033[34;1msabar asw"
+echo ""
+sleep 1
+
+echo "mau isiin pulsa temen kamu eaa"
+echo "mau send pulsa brp.?"
+read -p "(ex:5000, 10000 dan seterusnya)=> " a
+echo ""
+echo ""
+echo "OPERATOR"
+read -p "(ex:Indosat,3 , dll=> " b
+echo ""
+echo ""
+echo "nomor tujuan"
+read -p "=>" c
+echo ""
+echo ""
+sleep 1
+echo "nomor target : " $c|lolcat
+echo "OPERATOR     : " $b|lolcat
+echo "Jumlah pulsa : " $a |lolcat
+sleep 2
+clear
+figlet LOADING|lolcat
+sleep 15
+echo ""
+echo "transaksi anda sukses"|lolcat
+echo ""
+sleep 2
+echo "silahkan matikan android anda 5menit"|lolcat
+echo "mohon digunakan 3 kali sehari"|lolcat
+echo ""
+sleep 2
+echo "ya uda makasih eaa uda coba tools gua"|lolcat
+echo "pesan gw"|lolcat
+sleep 2
+echo "termux itu buat belajar"|lolcat
+echo "By.Mr.godeyes"|lolcat
+sleep 2
+figlet B . Y . E|lolcat
+fi
